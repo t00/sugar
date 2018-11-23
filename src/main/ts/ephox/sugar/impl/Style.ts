53ea558b2f83a206067e1a62a0df5783dba0c008
Michal Turecki <mturecki@speedwellsoftware.com>
2018-11-23 16:21:51 +0000

    Fixed getPropertyValue method missing on a style object if style is not a dom object

--- src/main/ts/ephox/sugar/impl/Style.ts
+++ src/main/ts/ephox/sugar/impl/Style.ts
@@ -2,7 +2,9 @@ import { HTMLElement } from "@ephox/dom-globals";
 
 // some elements, such as mathml, don't have style attributes
 var isSupported = function (dom: HTMLElement) {
-  return dom.style !== undefined;
+  return dom.style !== undefined 
+    && dom.style.constructor !== undefined
+	&& dom.style.constructor.name === 'CSSStyleDeclaration';
 };
 
 export default {
